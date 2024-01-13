60fd81d0a48eec158884c626cc40f4137e17950f
pan0mera <artess.ponomarev@mail.ru>
2024-01-13 16:31:14 +0300

    Немного поменял дизайн

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,17 @@
-import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
-import { Button } from '@mui/material';
-import cl from './index.module.scss';
 import { useAppContext } from '../../contexts/AppContext/AppContext';
+import cl from './index.module.scss';
 
 const Header = () => {
-    const { modalOpenCreate } = useAppContext();
+    const { currentUser, logout } = useAppContext();
 
     return (
         <header className={cl.header}>
             <div className={cl.header__content}>
                 <h1>Pan0mera's toDo lists</h1>
-                <Button variant="text" onClick={modalOpenCreate} className={cl.header__button}>
-                    <AddCircleOutlineOutlinedIcon /> Добавить
-                </Button>
+                <div className={cl.user}>
+                    <p>Привет, {currentUser?.login}</p>
+                    <p onClick={logout}>Выйти</p>
+                </div>
             </div>
         </header>
     );
--- src/components/ToDos/index.tsx
+++ src/components/ToDos/index.tsx
@@ -1,11 +1,20 @@
 import { useToDoContext } from '../../contexts/ToDoContext/ToDoContext';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import { Button } from '@mui/material';
+import { useAppContext } from '../../contexts/AppContext/AppContext';
 import Todo from './ToDo';
 import cl from './index.module.scss';
 
 const ToDos = () => {
     const { todos } = useToDoContext();
+    const { modalOpenCreate } = useAppContext();
+
     return (
         <div className={cl.todos}>
+            <Button variant="contained" onClick={modalOpenCreate} className={cl.todos__button}>
+                <AddCircleOutlineOutlinedIcon /> Добавить
+            </Button>
+
             {todos.length > 0 ? (
                 todos.map((el) => <Todo key={el.id} todo={el} />)
             ) : (
