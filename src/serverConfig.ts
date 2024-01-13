c7b877893449eb94785326e25378c278968aea2b
Pan0mera <54120155+ARTESS-PLAY@users.noreply.github.com>
2024-01-13 21:29:33 +0300

    Delete src/serverConfig.ts
    Изменил файлы чтобы загрузить всё на render.com

--- src/serverConfig.ts
+++
@@ -1,7 +0,0 @@
-export const config = {
-    port: process.env.PORT || 3005,
-    mongodbURL:
-        process.env.MONGODBURL ||
-        'mongodb+srv://admin:todosAdmin@todos.q41cgsb.mongodb.net/?retryWrites=true&w=majority',
-    secret: process.env.SECRET || 'pan0mera',
-};
